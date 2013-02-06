b0013a7f834373caf5ad48f49773320e11049a90
irasally <irasally@gmail.com>
2013-02-06 16:07:13 +0900

    [#43911327] 商品情報のチェック処理を追加した
    - 商品名と説明は必須
    - 価格は正の整数のみ
    - 同じ商品名は登録できない

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price, :image, :image_cache
   mount_uploader :image, ProductImageUploader
+
+  validates_presence_of :name, :description
+  validates_numericality_of :price, :greater_than_or_equal_to => 0, :only_integer => true
+  validates_uniqueness_of :name
 end
